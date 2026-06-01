0a9d274a880ace096ebbbe681c9485f6a871eca7
Shubhi Gupta <guptashubhi038@gmail.com>
2026-06-01 14:04:24 +0530

    DAY-1 uplodaded.

---
+++ DAY-1/1.c
@@ -0,0 +1,19 @@
+// C program to calculate the sum of first n natural numbers
+#include <stdio.h>
+
+int main()
+{
+    int n, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) 
+    {
+        sum =  sum + i;
+    }
+
+    printf("Sum of first %d natural numbers = %d\n", n, sum);
+
+    return 0;
+}

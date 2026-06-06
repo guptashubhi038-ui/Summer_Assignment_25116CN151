9d7731c206dad233e6edc708fc973999c752badd
Shubhi Gupta <guptashubhi038@gmail.com>
2026-06-06 12:17:54 +0530

    Add DAY-6 assignment

---
+++ DAY-6/21.c
@@ -0,0 +1,28 @@
+// program to convert a decimal number to binary.
+#include <stdio.h>
+
+int main() 
+{
+    int n, binary[32], i = 0;
+
+    printf("Enter a decimal number: ");
+    scanf("%d", &n);
+
+    int temp = n;
+
+    while (n > 0) 
+    {
+        binary[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+
+    printf("Binary of %d is: ", temp);
+
+    for (int j = i - 1; j >= 0; j--) 
+    {
+        printf("%d", binary[j]);
+    }
+
+    return 0;
+}
---
+++ DAY-6/24.c
@@ -0,0 +1,22 @@
+// program to calculate x raised to the power n.
+#include <stdio.h>
+
+int main() 
+{
+    int x, n, result = 1;
+
+    printf("Enter base: ");
+    scanf("%d", &x);
+
+    printf("Enter power: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) 
+    {
+        result = result * x;
+    }
+
+    printf("%d^%d = %d", x, n, result);
+
+    return 0;
+}

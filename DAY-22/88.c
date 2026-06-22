e817f03efefbc72b9b848c23678a70578f6b3ccb
Shubhi Gupta <guptashubhi038@gmail.com>
2026-06-22 23:52:02 +0530

    Add DAY-22 assignment

---
+++ DAY-22/88.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() 
+{
+    char str[100], newStr[100];
+    int i, j = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    for(i = 0; str[i] != '\0'; i++) 
+    {
+        if(str[i] != ' ') 
+        {
+            newStr[j] = str[i];
+            j++;
+        }
+    }
+
+    newStr[j] = '\0';
+
+    printf("String without spaces: %s", newStr);
+
+    return 0;
+}
